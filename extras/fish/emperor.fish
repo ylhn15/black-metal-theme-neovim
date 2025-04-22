dcd818333308a44a6096ee59cf11dd40db6a4de7
Andrea Schiavini <metalelf0@gmail.com>
2025-04-22 08:56:49 +0200

    fix: updates

--- extras/fish/emperor.fish
+++ extras/fish/emperor.fish
@@ -20,7 +20,7 @@ set -g number aaaaaa
 set -g operator 9b99a3 
 set -g property c1c1c1 
 set -g str 756482 
-set -g type 2d296a 
+set -g type A8A1DE 
 
 # Syntax Highlighting Colors
 set -g fish_color_normal c1c1c1
@@ -31,7 +31,7 @@ set -g fish_color_redirection aaaaaa
 set -g fish_color_end 9b99a3
 set -g fish_color_error 5f8787
 set -g fish_color_param c1c1c1
-set -g fish_color_valid_path 2d296a
+set -g fish_color_valid_path A8A1DE
 set -g fish_color_option 5f8787
 set -g fish_color_comment 505050
 set -g fish_color_selection --background=000000
@@ -46,7 +46,7 @@ set -g fish_color_search_match --background=000000
 
 # Completion Pager Colors
 set -g fish_pager_color_progress 5f8787
-set -g fish_pager_color_prefix 2d296a
+set -g fish_pager_color_prefix A8A1DE
 set -g fish_pager_color_completion c1c1c1
 set -g fish_pager_color_description 505050
 set -g fish_pager_color_selected_prefix 5f8787
--- extras/fish/impaled-nazarene.fish
+++ extras/fish/impaled-nazarene.fish
@@ -1,28 +1,44 @@
-# name: EMPEROR colors for Fish Themes
-# url: https://fishshell.com/docs/current/interactive.html#syntax-highlighting
+# name: IMPALED-NAZARENE colors for Fish
+# url: https://fishshell.com/docs/current/index.html
 # upstream: ${upstream}
 # author: Andrea Schiavini (original code by Casey Miller)
 
+# use in ~/.config/fish/conf.d/
+
 # preferred bg: 000000
 # alt background: 111113
+
+### Full palette. 
+### Colors defined in neomdern/palette/IMPALED-NAZARENE.lua
+set -g alt 5f8787 
+set -g constant aaaaaa 
+set -g comment 505050 
+set -g fg c1c1c1 
+set -g func 888888 
+set -g keyword 999999 
+set -g number aaaaaa 
+set -g operator 9b99a3 
+set -g property c1c1c1 
+set -g str DC2A22 
+set -g type B29740 
 
 # Syntax Highlighting Colors
 set -g fish_color_normal c1c1c1
 set -g fish_color_command 888888
 set -g fish_color_keyword 999999
-set -g fish_color_quote 756482
+set -g fish_color_quote DC2A22
 set -g fish_color_redirection aaaaaa
 set -g fish_color_end 9b99a3
 set -g fish_color_error 5f8787
 set -g fish_color_param c1c1c1
-set -g fish_color_valid_path A8A1DE
+set -g fish_color_valid_path B29740
 set -g fish_color_option 5f8787
 set -g fish_color_comment 505050
 set -g fish_color_selection --background=000000
 set -g fish_color_operator 9b99a3
 set -g fish_color_escape 999999
 set -g fish_color_autosuggestion 505050
-set -g fish_color_cwd 756482
+set -g fish_color_cwd DC2A22
 set -g fish_color_hostname aaaaaa
 set -g fish_color_status 5f8787
 set -g fish_color_cancel aaaaaa
@@ -30,7 +46,7 @@ set -g fish_color_search_match --background=000000
 
 # Completion Pager Colors
 set -g fish_pager_color_progress 5f8787
-set -g fish_pager_color_prefix A8A1DE
+set -g fish_pager_color_prefix B29740
 set -g fish_pager_color_completion c1c1c1
 set -g fish_pager_color_description 505050
 set -g fish_pager_color_selected_prefix 5f8787
